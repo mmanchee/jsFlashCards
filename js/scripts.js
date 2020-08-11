927d9c25c842d1429eab641c43fb4c1426e35182
Mike Manchee <mikemanchee@gmail.com>
2020-08-11 16:16:58 -0700

    add js to code project

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,124 @@ $(document).ready(function() {
     $("#javascript-word").toggle();
     $("#javascript-def").toggle();
   });
+
+  $("#operator").click(function() {
+    $("#operator-word").toggle();
+    $("#operator-def").toggle();
+  });
+
+  $("#variable").click(function() {
+    $("#variable-word").toggle();
+    $("#variable-def").toggle();
+  });
+
+  $("#varNameCon").click(function() {
+    $("#varNameCon-word").toggle();
+    $("#varNameCon-def").toggle();
+  });
+
+  $("#function").click(function() {
+    $("#function-word").toggle();
+    $("#function-def").toggle();
+  });
+
+  $("#method").click(function() {
+    $("#method-word").toggle();
+    $("#method-def").toggle();
+  });
+
+  $("#argument").click(function() {
+    $("#argument-word").toggle();
+    $("#argument-def").toggle();
+  });
+
+  $("#parameter").click(function() {
+    $("#parameter-word").toggle();
+    $("#parameter-def").toggle();
+  });
+
+  $("#return").click(function() {
+    $("#return-word").toggle();
+    $("#return-def").toggle();
+  });
+
+  $("#chainMeth").click(function() {
+    $("#chainMeth-word").toggle();
+    $("#chainMeth-def").toggle();
+  });
+
+  $("#string").click(function() {
+    $("#string-word").toggle();
+    $("#string-def").toggle();
+  });
+
+  $("#javascript").click(function() {
+    $("#javascript-word").toggle();
+    $("#javascript-def").toggle();
+  });
+
+  $("#boolean").click(function() {
+    $("#boolean-word").toggle();
+    $("#boolean-def").toggle();
+  });
+
+  $("#undefined").click(function() {
+    $("#undefined-word").toggle();
+    $("#undefined-def").toggle();
+  });
+
+  $("#nan").click(function() {
+    $("#nan-word").toggle();
+    $("#nan-def").toggle();
+  });
+
+  $("#null").click(function() {
+    $("#null-word").toggle();
+    $("#null-def").toggle();
+  });
+
+  $("#alert").click(function() {
+    $("#alert-word").toggle();
+    $("#alert-def").toggle();
+  });
+
+  $("#comment").click(function() {
+    $("#comment-word").toggle();
+    $("#comment-def").toggle();
+  });
+
+  $("#jquery").click(function() {
+    $("#jquery-word").toggle();
+    $("#jquery-def").toggle();
+  });
+
+  $("#attribute").click(function() {
+    $("#attribute-word").toggle();
+    $("#attribute-def").toggle();
+  });
+
+  $("#html").click(function() {
+    $("#html-word").toggle();
+    $("#html-def").toggle();
+  });
+
+  $("#css").click(function() {
+    $("#css-word").toggle();
+    $("#css-def").toggle();
+  });
+
+  $("#bootstrap").click(function() {
+    $("#bootstrap-word").toggle();
+    $("#bootstrap-def").toggle();
+  });
+
+  $("#htmlTag").click(function() {
+    $("#htmlTag-word").toggle();
+    $("#htmlTag-def").toggle();
+  });
+
+  $("#compare").click(function() {
+    $("#compare-word").toggle();
+    $("#compare-def").toggle();
+  });
 });
